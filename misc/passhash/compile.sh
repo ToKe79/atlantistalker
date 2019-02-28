6efbd7dffd853ee788f4507b300421edd32ed168
ToKe79 <tomas.kelemen@gmail.com>
2019-02-28 23:53:18 +0100

    passhash: update compile script

--- misc/passhash/compile.sh
+++ misc/passhash/compile.sh
@@ -2,7 +2,7 @@
 CC="ccache gcc"
 PROJECT="passhash"
 LIBS="-lmysqlclient -ldl -rdynamic $(xml2-config --libs)"
-CCFLAGS="-Wall -ggdb -pedantic -I. $(xml2-config --cflags)"
+CFLAGS="-Wall -Wextra -Wformat=2 -ggdb -pedantic -I. $(xml2-config --cflags)"
 IN="passhash.c"
 
-${CC} ${CCFLAGS} ${IN} -o ${PROJECT} ${LIBS}
+${CC} ${CFLAGS} ${IN} -o ${PROJECT} ${LIBS}
