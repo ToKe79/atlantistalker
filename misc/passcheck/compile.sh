93a9f098a6f87a53ad42ddad47e18427e94366b1
ToKe79 <tomas.kelemen@gmail.com>
2019-02-28 23:50:10 +0100

    passcheck: changes due to config.h(.sample)

--- misc/passcheck/compile.sh
+++ misc/passcheck/compile.sh
@@ -2,7 +2,11 @@
 CC="ccache gcc"
 PROJECT="passcheck"
 LIBS="-lmysqlclient -ldl -rdynamic $(xml2-config --libs)"
-CCFLAGS="-Wall -ggdb -pedantic -I. $(xml2-config --cflags)"
+CFLAGS="-Wall -Wextra -Wformat=2 -ggdb -pedantic -I. $(xml2-config --cflags)"
 IN="passcheck.c"
 
-${CC} ${CCFLAGS} ${IN} -o ${PROJECT} ${LIBS}
+if [ "${HAVE_CONFIG}" = "1" ] ; then
+	CFLAGS="-DHAVE_CONFIG ${CFLAGS}"
+fi
+
+${CC} ${CFLAGS} ${IN} -o ${PROJECT} ${LIBS}
--- misc/passcheck/passcheck.c
+++ misc/passcheck/passcheck.c
@@ -23,54 +23,33 @@ char *expand_pass(char *pass) {
 
 int main(int argc,char *argv[]) {
 
-	FILE *fp;
-	char line[200];
-	char adb_name[30];
-	char adb_user[30];
-	char adb_pass[30];
 	char pass_passed[]="PW_CHECK_PASSED";
 	char pass_failed[]="PW_CHECK_FAILED";
 
-	if (argc!=4) {
-		fprintf(stderr,"Pouzi: %s <config_file> <user> <pass>\n\nOveri uzivatelove heslo.\n\nVystup:\n   %s ak heslo sedi, alebo\n   %s ak heslo nesedi.\n\n",argv[0],pass_passed,pass_failed);
-		return 1;
-	}
-
-	if (!(fp=fopen(argv[1],"r"))) {
-		fprintf(stderr,"CHYBA: nenajdeny subor '%s' s konfiguraciou!\n",argv[1]);
+	if (argc!=3) {
+		fprintf(stderr,"Pouzi: %s <user> <pass>\n\nOveri uzivatelove heslo.\n\nVystup:\n   %s ak heslo sedi, alebo\n   %s ak heslo nesedi.\n\n",argv[0],pass_passed,pass_failed);
 		return 1;
 	}
 
 	/* Nickname must begin with an uppercase letter */
-	argv[2][0]=toupper(argv[2][0]);
-
-	fgets(line,199,fp);
-	line[strlen(line)-1]='\0';
-	strncpy(adb_name,line,29);
-	fgets(line,199,fp);
-	line[strlen(line)-1]='\0';
-	strncpy(adb_user,line,29);
-	fgets(line,199,fp);
-	line[strlen(line)-1]='\0';
-	fclose(fp);
-	strncpy(adb_pass,line,29);
+	argv[1][0]=toupper(argv[1][0]);
 
 	if (mysql_init(&mysql)==NULL) {
 		fprintf(stderr,"MYSQL: Nedostatok pamate pre vytvorenie objektu!\n");
 		return 1;
 	}
 
-	if (!mysql_real_connect(&mysql,DBHOSTNAME,adb_user,adb_pass,NULL,0,NULL,0)) {
+	if (!mysql_real_connect(&mysql,DBHOST,DBUSER,DBPASS,NULL,0,NULL,0)) {
 		fprintf(stderr,"MYSQL: Zlyhalo pripojenie k databaze: %s\n",mysql_error(&mysql));
 		return 1;
 	}
 
-	if (mysql_select_db(&mysql,adb_name)) {
+	if (mysql_select_db(&mysql,DBNAME)) {
 		fprintf(stderr,"MYSQL: Nemozno vybrat zakladnu databazu!\n");
 		return 1;
         }
 
-	sprintf(query,"SELECT `passwd` FROM `users` WHERE `username` = '%s'",argv[2]);
+	sprintf(query,"SELECT `passwd` FROM `users` WHERE `username` = '%s'",argv[1]);
 
 	if(!(result=mysql_result(query))) {
 		fprintf(stderr,"MYSQL: Zlyhal dopyt '%s' na databazu!\n",query);
@@ -84,10 +63,10 @@ int main(int argc,char *argv[]) {
 
 	mysql_free_result(result);
 
-	if(strcmp((char *)md5_crypt(expand_pass(argv[3]),argv[2]),row[0]) != 0) {
-		printf(pass_failed);
+	if(strcmp((char *)md5_crypt(expand_pass(argv[2]),argv[1]),row[0]) != 0) {
+		printf("%s",pass_failed);
 	} else {
-		printf(pass_passed);
+		printf("%s",pass_passed);
 	}
 
 	return 0;
