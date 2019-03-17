e1d8ca398cd7c1cffe3b1d3db32c72b5f989d964
ToKe79 <tomas.kelemen@gmail.com>
2019-03-17 19:54:21 +0100

    atl-libapi: styling/cleanup

--- atl-libapi.c
+++ atl-libapi.c
@@ -11,33 +11,31 @@
 #include "atl-head.h"
 #include "atl-libapi.h"
 #include "atl-libs.h"
-
 #ifdef __cplusplus
 /* Exportovane funkcie pre C */
 extern "C" {
 	int api_room_info(char *);
 	int api_room_create(char *);
 	int api_room_destroy(char *, char *);
-	int api_room_link(char *, char *);
-	int api_room_unlink(char *, char *);
-	int api_room_write(char *, char *);
-	int api_room_set_desc(char *, int, char *);
+	int api_room_link(char *,char *);
+	int api_room_unlink(char *,char *);
+	int api_room_write(char *,char *);
+	int api_room_set_desc(char *,int, char *);
 	int api_room_get_first(char *);
-	int api_room_get_next(char *, char *);
-	int api_room_get_user_first(char *, char *);
-	int api_room_get_user_next(char *, char *, char *);
-	int api_user_set_dead(char *, char *);
+	int api_room_get_next(char *,char *);
+	int api_room_get_user_first(char *,char *);
+	int api_room_get_user_next(char *,char *,char *);
+	int api_user_set_dead(char *,char *);
 	int api_user_get_idletime(char *);
-	int api_user_write(char *, char *);
-	int api_user_get_pad(char *, char *, int);
+	int api_user_write(char *,char *);
+	int api_user_get_pad(char *,char *,int);
 	int api_user_get_first(char *);
-	int api_user_get_next(char *, char *);
-	int api_command_reg(int, int, int, int, const char *, int, int, int (*)(char *,char *), int, int, int, int);
-	int api_fnc_reg(unsigned int, struct hook_s *);
+	int api_user_get_next(char *,char *);
+	int api_command_reg(int,int,int,int,const char *,int,int,int (*)(char *,char *),int,int,int,int);
+	int api_fnc_reg(unsigned int,struct hook_s *);
 	int api_fnc_unreg(unsigned int);
 }
 #endif
-
 
 /****f* API/api_room_info
  *  NAME
@@ -52,12 +50,13 @@ extern "C" {
  *  BUGS
  *    Momentalne je tato funkcia vhodna jedine na skontrolovanie ci existuje miestnost s takymto nazvom a ziadne "info" nevracia.
  *****/
-int api_room_info(char *name) {
-  RM_OBJECT rm;
-  
-  if (!(rm = get_room (name, NULL)))
-    return -1;
-  return 0;
+int api_room_info(char *name)
+{
+	RM_OBJECT rm;
+
+	if (!(rm=get_room(name,NULL)))
+		return -1;
+	return 0;
 }
 
 /****f* API/api_room_create
@@ -71,10 +70,13 @@ int api_room_info(char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_create(char *name) {
-  RM_OBJECT rm;
-  if (!(rm = create_room (name, name))) return -1;
-  return 0;
+int api_room_create(char *name)
+{
+	RM_OBJECT rm;
+
+	if (!(rm=create_room(name, name)))
+		return -1;
+	return 0;
 }
 
 /****f* API/api_room_destroy
@@ -89,24 +91,24 @@ int api_room_create(char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_destroy(char *name, char *qmsg) {
-  RM_OBJECT rm;
-  UR_OBJECT u, n;
-  
-  if (!(rm = get_room (name, NULL)))
-    return -1;
+int api_room_destroy(char *name,char *qmsg)
+{
+	RM_OBJECT rm;
+	UR_OBJECT u,n;
 
-  u=user_first;
-  while (u!=NULL) {
-    n=u->next;
-    if (u->room==rm) {
-      logout_user (u, qmsg);
-      inyjefuc = 1;
-    }
-    u=n;
-  }
-  /* destruct_room(rm); */ /* oops nemame */
-  return 0;
+	if (!(rm=get_room(name,NULL)))
+		return -1;
+	u=user_first;
+	while (u!=NULL) {
+		n=u->next;
+		if (u->room==rm) {
+			logout_user(u,qmsg);
+			inyjefuc=1;
+		}
+		u=n;
+	}
+	/* destruct_room(rm); */ /* oops nemame */
+	return 0;
 }
 
 /****f* API/api_room_link
@@ -121,13 +123,16 @@ int api_room_destroy(char *name, char *qmsg) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_link(char *name1, char *name2) {
-  RM_OBJECT rm1, rm2;
-  
-  if (!(rm1 = get_room (name1, NULL))) return -1;
-  if (!(rm2 = get_room (name2, NULL))) return -1;
-  link_room (rm1, rm2);
-  return 0;
+int api_room_link(char *name1,char *name2)
+{
+	RM_OBJECT rm1,rm2;
+
+	if (!(rm1=get_room(name1, NULL)))
+		return -1;
+	if (!(rm2=get_room(name2, NULL)))
+		return -1;
+	link_room(rm1,rm2);
+	return 0;
 }
 
 /****f* API/api_room_unlink
@@ -142,13 +147,16 @@ int api_room_link(char *name1, char *name2) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_unlink(char *name1, char *name2) {
-  RM_OBJECT rm1, rm2;
-  
-  if (!(rm1 = get_room (name1, NULL))) return -1;
-  if (!(rm2 = get_room (name2, NULL))) return -1;
-  unlink_room (rm1, rm2);
-  return 0;
+int api_room_unlink(char *name1,char *name2)
+{
+	RM_OBJECT rm1,rm2;
+
+	if (!(rm1=get_room (name1,NULL)))
+		return -1;
+	if (!(rm2=get_room (name2,NULL)))
+		return -1;
+	unlink_room(rm1,rm2);
+	return 0;
 }
 
 /****f* API/api_room_write
@@ -163,13 +171,14 @@ int api_room_unlink(char *name1, char *name2) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_write(char *name, char *text) {
-  RM_OBJECT rm;
-  
-  if (!(rm = get_room (name, NULL)))
-    return -1;
-  write_room(rm, text);
-  return 0;
+int api_room_write(char *name,char *text)
+{
+	RM_OBJECT rm;
+
+	if (!(rm=get_room(name,NULL)))
+		return -1;
+	write_room(rm,text);
+	return 0;
 }
 
 /****f* API/api_room_set_desc
@@ -185,30 +194,25 @@ int api_room_write(char *name, char *text) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_set_desc(char *name, int lng, char *desc) {
-  RM_OBJECT rm;
+int api_room_set_desc(char *name,int lng,char *desc)
+{
+	RM_OBJECT rm;
 
-  if (!(rm = get_room (name, NULL)))
-    return -1;
-
-  if ((lng < 0) || (lng > 2)) {
-	  return -2;
-  }
-
-  if ((lng == 0) || (lng == 2)) {
-	  if (rm->desc_sk != NULL) {
-		  free (rm->desc_sk);
-	  }
-	  rm->desc_sk=strdup(desc);
-  }
-
-  if ((lng == 1) || (lng == 2)) {
-	  if (rm->desc_en != NULL)
-		  free (rm->desc_en);
-	  rm->desc_en=strdup(desc);
-  }
-
-  return 0;
+	if (!(rm=get_room(name, NULL)))
+		return -1;
+	if ((lng<0) || (lng>2))
+		return -2;
+	if ((lng==0) || (lng==2)) {
+		if (rm->desc_sk!=NULL)
+			free(rm->desc_sk);
+		rm->desc_sk=strdup(desc);
+	}
+	if ((lng==1) || (lng==2)) {
+		if (rm->desc_en!=NULL)
+			free(rm->desc_en);
+		rm->desc_en=strdup(desc);
+	}
+	return 0;
 }
 
 /****f* API/api_room_get_first
@@ -222,14 +226,11 @@ int api_room_set_desc(char *name, int lng, char *desc) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_get_first(char *dst) {
-	/* RM_OBJECT rm; */
-
-	if (room_first == NULL) {
+int api_room_get_first(char *dst)
+{
+	if (room_first==NULL)
 		return -1;
-	}
-	strncpy(dst, room_first->name, ROOM_NAME_LEN);
-
+	strncpy(dst,room_first->name,ROOM_NAME_LEN);
 	return 0;
 }
 
@@ -245,20 +246,17 @@ int api_room_get_first(char *dst) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_get_next(char *dst, char *name) {
+int api_room_get_next(char *dst,char *name)
+{
 	RM_OBJECT rm;
 	char tmpname[ROOM_NAME_LEN + 1];
-	strncpy(tmpname, name, ROOM_NAME_LEN);
 
-	if (!(rm = get_room (tmpname, NULL))) {
+	strncpy(tmpname,name,ROOM_NAME_LEN);
+	if (!(rm=get_room(tmpname,NULL)))
 		return -1;
-	}
-	if (rm->next == NULL) {
+	if (rm->next==NULL)
 		return -1;
-	}
-
-	strncpy(dst, rm->next->name, ROOM_NAME_LEN);
-
+	strncpy(dst,rm->next->name,ROOM_NAME_LEN);
 	return 0;
 }
 
@@ -274,19 +272,19 @@ int api_room_get_next(char *dst, char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_get_user_first(char *dst, char *name) {
+int api_room_get_user_first(char *dst,char *name)
+{
 	UR_OBJECT u;
-	char tmpname[USER_NAME_LEN + 1];
+	char tmpname[USER_NAME_LEN+1];
 
-	u = user_first;
+	u=user_first;
 	while (u) {
-		if (u->room && !strcasecmp(u->room->name, name)) {
-			strncpy(dst, u->name, sizeof(tmpname));
+		if (u->room && !strcasecmp(u->room->name,name)) {
+			strncpy(dst,u->name,sizeof(tmpname));
 			return 0;
 		}
 		u = u->next;
 	}
-
 	return -1;
 }
 
@@ -303,25 +301,22 @@ int api_room_get_user_first(char *dst, char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_room_get_user_next(char *dst, char *roomname, char *username) {
+int api_room_get_user_next(char *dst,char *roomname,char *username)
+{
 	UR_OBJECT u;
-	char tmpname[USER_NAME_LEN + 1];
+	char tmpname[USER_NAME_LEN+1];
 
-	strncpy(tmpname, username, USER_NAME_LEN);
-
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,username,USER_NAME_LEN);
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-
-	u = user_first;
+	u=user_first;
 	while (u) {
-		if (u->room && !strcasecmp(u->room->name, roomname)) {
-			strncpy(dst, u->name, USER_NAME_LEN);
+		if (u->room && !strcasecmp(u->room->name,roomname)) {
+			strncpy(dst,u->name,USER_NAME_LEN);
 			return 0;
 		}
-		u = u->next;
+		u=u->next;
 	}
-
 	return -1;
 }
 
@@ -338,20 +333,17 @@ int api_room_get_user_next(char *dst, char *roomname, char *username) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_get_pad(char *dst, char *name, int pad) {
+int api_user_get_pad(char *dst,char *name,int pad)
+{
 	UR_OBJECT u;
 	char *s;
-	char tmpname[USER_NAME_LEN + 1];
-	strncpy(tmpname, name, USER_NAME_LEN);
+	char tmpname[USER_NAME_LEN+1];
 
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,name,USER_NAME_LEN);
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-	
-	s = sklonuj(u, pad);
-
-	strncpy(dst, s, USER_NAME_LEN);
-
+	s=sklonuj(u, pad);
+	strncpy(dst,s,USER_NAME_LEN);
 	return 0;
 }
 
@@ -367,19 +359,15 @@ int api_user_get_pad(char *dst, char *name, int pad) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_set_dead(char *name, char *text) {
+int api_user_set_dead(char *name,char *text)
+{
 	UR_OBJECT u;
-	char tmpname[USER_NAME_LEN + 1];
-	strncpy(tmpname, name, sizeof(tmpname));
+	char tmpname[USER_NAME_LEN+1];
 
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,name,sizeof(tmpname));
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-
-	/* u->dead = DEAD_CUSTOM_MESSAGE; */
-	/* snprintf(u->death_custom_message, 255, "%s", text); */
 	quit_user(u, text);
-	
 	return 0;
 }
 
@@ -394,12 +382,11 @@ int api_user_set_dead(char *name, char *text) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_get_first(char *dst) {
-	if (user_first == NULL) {
+int api_user_get_first(char *dst)
+{
+	if (user_first==NULL)
 		return -1;
-	}
-	strncpy(dst, user_first->name, USER_NAME_LEN);
-
+	strncpy(dst,user_first->name,USER_NAME_LEN);
 	return 0;
 }
 
@@ -415,20 +402,17 @@ int api_user_get_first(char *dst) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_get_next(char *dst, char *name) {
+int api_user_get_next(char *dst,char *name)
+{
 	UR_OBJECT u;
-	char tmpname[USER_NAME_LEN + 1];
-	strncpy(tmpname, name, USER_NAME_LEN);
+	char tmpname[USER_NAME_LEN+1];
 
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,name,USER_NAME_LEN);
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-	if (u->next == NULL) {
+	if (u->next==NULL)
 		return -1;
-	}
-
-	strncpy(dst, u->next->name, USER_NAME_LEN);
-
+	strncpy(dst,u->next->name,USER_NAME_LEN);
 	return 0;
 }
 
@@ -443,15 +427,14 @@ int api_user_get_next(char *dst, char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_get_idletime(char *name) {
+int api_user_get_idletime(char *name)
+{
 	UR_OBJECT u;
 	char tmpname[USER_NAME_LEN + 1];
-	strncpy(tmpname, name, USER_NAME_LEN);
 
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,name,USER_NAME_LEN);
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-
 	return u->idletime;
 }
 
@@ -467,15 +450,14 @@ int api_user_get_idletime(char *name) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_user_write(char *name, char* text) {
+int api_user_write(char *name,char* text)
+{
 	UR_OBJECT u;
 	char tmpname[USER_NAME_LEN + 1];
-	strncpy(tmpname, name, sizeof(tmpname));
 
-	if (!(u = get_user_exact (tmpname))) {
+	strncpy(tmpname,name,sizeof(tmpname));
+	if (!(u=get_user_exact(tmpname)))
 		return -1;
-	}
-
 	write_user(u, text);
 	return 0;
 }
@@ -545,22 +527,22 @@ int api_command_reg (
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_fnc_reg(unsigned int libid, struct hook_s *hooks) {
-  int i;
+int api_fnc_reg(unsigned int libid,struct hook_s *hooks)
+{
+	int i;
 
-  if (hooks == NULL) {
-	  return -1;
-  }
-
-  for (i=0;i<MAXLIBS;i++) {
-    if (!lib_array[i].lib_handle) continue;
-    if (lib_array[i].lib_id==libid) break;
-  }
-  if (i==MAXLIBS) return -1;
-
-  memcpy((void *)(&(lib_array[i].hooks)), (void *) hooks, sizeof(struct hook_s));
-
-  return 0;
+	if (hooks==NULL)
+		return -1;
+	for (i=0;i<MAXLIBS;i++) {
+		if (!lib_array[i].lib_handle)
+			continue;
+		if (lib_array[i].lib_id==libid)
+			break;
+	}
+	if (i==MAXLIBS)
+		return -1;
+	memcpy((void *)(&(lib_array[i].hooks)),(void *)hooks,sizeof(struct hook_s));
+	return 0;
 }
 
 /****f* API/api_fnc_unreg
@@ -574,17 +556,18 @@ int api_fnc_reg(unsigned int libid, struct hook_s *hooks) {
  *    *  0 - v pripade uspechu
  *    * -1 - ak nastala chyba
  *****/
-int api_fnc_unreg(unsigned int libid) {
-  int i;
-  
-  for (i=0;i<MAXLIBS;i++) {
-    if (!lib_array[i].lib_handle) continue;
-    if (lib_array[i].lib_id==libid) break;
-  }
-  if (i==MAXLIBS) return -1;
+int api_fnc_unreg(unsigned int libid)
+{
+	int i;
 
-  memset((void *) &(lib_array[i].hooks), 0, sizeof(struct hook_s));
-
-  return 0;
+	for (i=0;i<MAXLIBS;i++) {
+		if (!lib_array[i].lib_handle)
+			continue;
+		if (lib_array[i].lib_id==libid)
+			break;
+	}
+	if (i==MAXLIBS)
+		return -1;
+	memset((void *)&(lib_array[i].hooks),0,sizeof(struct hook_s));
+	return 0;
 }
-
--- atl-libapi.h
+++ atl-libapi.h
@@ -1,5 +1,3 @@
-#ifndef _ATL_LIBAPI_H
-#define _ATL_LIBAPI_H
 /*
  *  NAME
  *    atl-libapi.h
@@ -10,35 +8,35 @@
  *    (c) Martin "Wolcaan" Karas wolcano@talker.sk
  */
 
-#define LIBERR_ALREADY_LOADED		-1
-#define LIBERR_NOT_LOADED		-2
-#define LIBERR_LIBARRAY_EXHAUSTED	-3
-#define LIBERR_DLOPEN_ERROR		-4
-#define LIBERR_MISSING_LIBID		-5
-#define LIBERR_MISSING_INIT		-6
-#define LIBERR_MISSING_DESTROY		-7
-#define LIBERR_INITIALIZATION		-8
-#define LIBERR_OTHER		-9
-#define LIBERR_BAD_NAME		-10
-#define LIBERR_OK			0
+#ifndef _ATL_LIBAPI_H
+#define _ATL_LIBAPI_H
+#define LIBERR_ALREADY_LOADED -1
+#define LIBERR_NOT_LOADED -2
+#define LIBERR_LIBARRAY_EXHAUSTED -3
+#define LIBERR_DLOPEN_ERROR -4
+#define LIBERR_MISSING_LIBID -5
+#define LIBERR_MISSING_INIT -6
+#define LIBERR_MISSING_DESTROY -7
+#define LIBERR_INITIALIZATION -8
+#define LIBERR_OTHER -9
+#define LIBERR_BAD_NAME -10
+#define LIBERR_OK 0
 
 struct hook_s {
-  int (*lib_socket_read)(char *, int);
-  int (*lib_socket_write)(char *, int);
-  int (*lib_heartbeat)();
-  int (*lib_room_create)(char *);
-  int (*lib_room_destroy)(char *, char *);
-  int (*lib_room_link)(char *, char *);
-  int (*lib_room_unlink)(char *, char *);
-  int (*lib_command)(char *, char *);
-
-  /*
-  int (*lib_login)(char *);
-  int (*lib_logout)(char *);
-  int (*lib_change_param)(int, char *);
-  int (*lib_room_write)(char *, char *);
-  int (*lib_room_set_desc)(char *, int, char *);
-  */
+	int (*lib_socket_read)(char *, int);
+	int (*lib_socket_write)(char *, int);
+	int (*lib_heartbeat)();
+	int (*lib_room_create)(char *);
+	int (*lib_room_destroy)(char *, char *);
+	int (*lib_room_link)(char *, char *);
+	int (*lib_room_unlink)(char *, char *);
+	int (*lib_command)(char *, char *);
+	/*
+	int (*lib_login)(char *);
+	int (*lib_logout)(char *);
+	int (*lib_change_param)(int, char *);
+	int (*lib_room_write)(char *, char *);
+	int (*lib_room_set_desc)(char *, int, char *);
+	*/
 };
-
 #endif
